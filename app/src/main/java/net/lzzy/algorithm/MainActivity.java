82aa6378dddd310973eab6dd6cdf5298f6c87ab3
YangXiongLin <273148388@qq.com>
2019-06-13 15:33:10 +0800

    直接插入排序

--- app/src/main/java/net/lzzy/algorithm/MainActivity.java
+++ app/src/main/java/net/lzzy/algorithm/MainActivity.java
@@ -35,14 +35,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 displayItems(edtItems);
                 break;
             case R.id.activity_main_btn_sort:
-                directSort();
+//                directSort();
+                insertSort();
                 displayItems(tvResult);
                 break;
             default:
                 break;
         }
     }
-
     private void displayItems(TextView tv) {
         String display = "";
         for (Integer i : items) {
@@ -51,25 +51,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         display = display.substring(0, display.length() - 1);
         tv.setText(display);
     }
-
     private void directSort() {
-                    //todo:直接选择排序的具体实现
-                    //todo:for循环
-                    //分为有序区和无序区，每一趟排序都在无序区依次对比，记录对比区域最下元素的位置，
-                    //然后把无序区第一元素所记录的最小元素进行交换，无序区少一个，有序区多一个，循环往复直至无序区
-                    //元素数量为0
+        //todo:直接选择排序的具体实现
+        //todo:for循环
+        //分为有序区和无序区，每一趟排序都在无序区依次对比，记录对比区域最下元素的位置，
+        //然后把无序区第一元素所记录的最小元素进行交换，无序区少一个，有序区多一个，循环往复直至无序区
+        //元素数量为0
         ///
 
-                    for (int i=0;i<items.length-1;i++){
-                        int minpos=i;
-                        for (int j=i+1;j<items.length;j++){
-                            if (items[minpos].compareTo(items[j])>0){
-                                minpos = j;
+        for (int i=0;i<items.length-1;i++){
+            int minpos=i;
+            for (int j=i+1;j<items.length;j++){
+                if (items[minpos].compareTo(items[j])>0){
+                    minpos = j;
                 }
-                            swap(minpos,i);
+                swap(minpos,i);
             }
         }
     }
+    private void insertSort(){
+        for (int i=1;i<items.length;i++) {
+            int j=i-1;
+            if (items[j].compareTo(items[i])<0){
+                continue;
+            }
+            Integer ccc=items[i];
+            while (j>=0&&items[j].compareTo(ccc)>0){
+                items[j+1]=items[j];
+                j--;
+            }
+            items[j+1]=ccc;
+            }
+        }
+
 
     private void swap(int m, int n) {
         int tmp=items[m];
@@ -83,5 +97,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for (int i = 0; i < items.length; i++) {
             items[i] = generator.nextInt(99);
         }
+
     }
 }
